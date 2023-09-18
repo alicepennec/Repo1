0946d9ce7cd1c0adcfe0c77e6922af55f7ee27c8
Alice <alicepennec@gmail.com>
2023-09-18 22:05:33 +0200

    HTML and CSS done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,36 @@
 class Game {
+  constructor() {
+    this.startScreen = document.getElementById('game-intro')
+    this.gameScreen = document.getElementById('game-screen')
+    this.gameEndScreen = document.getElementById('game-end')
+    this.player = null
+    this.height = 500
+    this.width = 1000
+    this.score = 0
+    this.gameIsOver = false
+
+  }
+
+  start() {
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
     
+    this.gameLoop();
+  }
+  gameLoop() {
+   if (this.gameIsOver) {
+    return;
+   }
+
+   this.update();
+
+   window.requestAnimationFrame(() => this.gameLoop());
+  }
+
+  update() {
+    
+  }
 }
